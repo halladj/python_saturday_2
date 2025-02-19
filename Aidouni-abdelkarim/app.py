bd9c803fe2d184e3a77eedc6d3ba7b7c00e0cdcf
halladj <halladj00@gmail.com>
2025-02-19 16:13:59 +0100

    fixed

--- Aidouni-abdelkarim/app.py
+++ Aidouni-abdelkarim/app.py
@@ -1,21 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
 @app.route("/about")
-
-
 def about():
     return render_template("about.html")
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
 @app.route("/prodect/<id>")
 def prodect(id):
     prodect = ["book", "pen", "phone"]
     x= prodect[int(id)]
     return {"id":x}
-@app.route("/submit/")
+
+@app.route("/submit/", methods=["POST"])
 def submit():
     name=request.form.get("name")
     password=request.form.get("password")
