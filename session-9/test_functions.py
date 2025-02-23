842db63a7b4cf11fa389aa96872609c93b9616e1
abdelkarim-aidouni <aidouni.krm2004@icloud.com>
2025-02-23 14:16:00 +0100

    add

--- session-9/test_functions.py
+++ session-9/test_functions.py
@@ -14,6 +14,20 @@ class TestFunctions(unittest.TestCase):
         self.list.Add(1)
         self.list.Add(2)
         self.assertEqual(self.list.Access(), 2)
+    def test_find(self):
+        self.list.Add(1)
+        self.assertEqual(self.list.Find(1),True )
+        self.assertEqual(self.list.Find(2), False)
+    def test_max(self):
+        self.list.Add(-1)
+        self.list.Add(-2)
+        self.assertEqual(self.list.max(), -1)
+    def test_min(self):
+        self.list.Add(1)
+        self.list.Add(2)
+        self.assertEqual(self.list.min(), 1)
+
+
 
 if __name__ == "__main__":
     unittest.main()
