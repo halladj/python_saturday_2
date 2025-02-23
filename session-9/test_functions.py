b52b05c716f9be3346d8eaf59cad0cfa3e69588f
mouatez <antecproxies@gmail.com>
2025-02-23 14:02:39 +0100

    mouatez push

--- session-9/test_functions.py
+++ session-9/test_functions.py
@@ -5,15 +5,20 @@ class TestFunctions(unittest.TestCase):
     def setUp(self):
         self.list = special_list.SpecialList()
 
-    def test_add(self):
-        self.list.Add(1)
-        self.assertEqual(self.list.my_list, [1])
+    # def test_add(self):
+    #     self.list.Add(1)
+    #     self.assertEqual(self.list.my_list, [1])
 
-    def test_access(self):
-        self.assertEqual(self.list.Access(), None)
-        self.list.Add(1)
-        self.list.Add(2)
-        self.assertEqual(self.list.Access(), 2)
+    # def test_access(self):
+    #     self.assertEqual(self.list.Access(), None)
+    #     self.list.Add(1)
+    #     self.list.Add(2)
+    #     self.assertEqual(self.list.Access(), 2)
+
+    def test_Find(self):
+        self.list.Add(5)
+        self.assertTrue(self.list.Find(5))
+        self.assertFalse(self.list.Find(9))
 
 if __name__ == "__main__":
     unittest.main()
